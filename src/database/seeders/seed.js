6c9c539d69d7bf59af5de95f0a77abe760b49144
Luchoragusa <luchito.rc4@gmail.com>
2023-05-10 17:48:48 -0300

    add seed folder

---
+++ src/database/seeders/seed.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcryptjs'); 
+
+// En este caso descripcion es el nombre del tipo de parametro de Tipo, la id la asigna sola sequelize
+const tipos = [
+    {descripcion: "Leve"},  
+    {descripcion: "Moderado"},
+]
+
+
+module.exports = {
+    tipos,
+}
