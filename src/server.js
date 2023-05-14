c838ea9644677205c42ca58e49eaaa5bf633095f
Luchoragusa <luchito.rc4@gmail.com>
2023-05-13 21:06:55 -0300

    set schedule

--- src/server.js
+++ src/server.js
@@ -15,6 +15,21 @@ const { sendMessage } = require('./utilities/bot')
 // Uso del bot
 sendMessage(1612650323, 'Ola')
 
+// Seteo la rutina
+
+const schedule = require('node-schedule')
+const rule = new schedule.RecurrenceRule()
+
+const times = []
+for (let i = 0; i < 60; i = i + 10) { // Creo un array con los tiempos en los que se va a ejecutar la rutina
+  times.push(i)
+}
+rule.second = times // Seteo la rutina para que se ejecute cada 10 segundos
+
+schedule.scheduleJob(rule, function () {
+  console.log('The answer to life, the universe, and everything!' + new Date())
+})
+
 // Settings
 app.use(cors())
 app.use(express.json())
