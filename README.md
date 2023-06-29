a921b63792ea1b7d73ea48e83ed15dbbe954c665
“Luchoragusa” <luchito.rc4@gmail.com>
2023-06-29 15:46:48 -0300

    documentacion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FrioStats-back
+
+
+- [Documentacion de EndPoints](https://documenter.getpostman.com/view/23041568/2s93zB4gC3)
--- src/controllers/models/usuario.controller.js
+++ src/controllers/models/usuario.controller.js
@@ -41,7 +41,6 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body
-  console.log(req.body)
   try {
     await Usuario.findOne({ where: { email } })
       .then((user) => {
