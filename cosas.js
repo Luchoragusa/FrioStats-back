acd13cae9937dc40c03aea17c5b1aa7cdf7c0384
Luchoragusa <luchito.rc4@gmail.com>
2023-06-29 20:47:47 -0300

    comment cosas

--- cosas.js
+++ cosas.js
@@ -1,16 +1,15 @@
-Desde el [server.js] dentro del (schedulejob) llamo al [verificator.js]
+// Desde el [server.js] dentro del (schedulejob) llamo al [verificator.js]
 
--> el schedulejob es lo que se ejecuta cada x tiempo, eso simula lo que seria una api con la llegada de los datso de las maquinas
+// -> el schedulejob es lo que se ejecuta cada x tiempo, eso simula lo que seria una api con la llegada de los datso de las maquinas
 
--> 1. Se generan las mediciones, llamando al [measurementGenerator.js]
---> 1. Se traen TODAS las maquinas cargadas
---> 2. En un foreach las recorro y por cada una le paso la id de maquina al metodo (obtenerMediciones) dentro de [measurementGenerator.js]
---> 3. Se genera la medicion de cada una y se guarda en la DB
+// -> 1. Se generan las mediciones, llamando al [measurementGenerator.js]
+// --> 1. Se traen TODAS las maquinas cargadas
+// --> 2. En un foreach las recorro y por cada una le paso la id de maquina al metodo (obtenerMediciones) dentro de [measurementGenerator.js]
+// --> 3. Se genera la medicion de cada una y se guarda en la DB
 
--> 2. Dentro del foreach dsp de que se guardaron las mediciones en la DB, obtengo los parametros (los minimos y los maximos) y la importancia de los parametros (Leve - Grave)
--> 3. Comparo parametro por parametro y si corresponde genero la notificacion correspondiente
+// -> 2. Dentro del foreach dsp de que se guardaron las mediciones en la DB, obtengo los parametros (los minimos y los maximos) y la importancia de los parametros (Leve - Grave)
+// -> 3. Comparo parametro por parametro y si corresponde genero la notificacion correspondiente
 
+// Obligatoriamente, por cada MaquinaSucursal que yo creo debo crear sus parametros
 
-Obligatoriamente, por cada MaquinaSucursal que yo creo debo crear sus parametros
-
-= Ver el tema de cuando es leve y cuando es Grave, osea si ya se va de los limites lo tengo q informar de acuerdo a como esta en importanciaParametro
+// = Ver el tema de cuando es leve y cuando es Grave, osea si ya se va de los limites lo tengo q informar de acuerdo a como esta en importanciaParametro
