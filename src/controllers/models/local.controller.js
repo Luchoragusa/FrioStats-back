8a44c1cfa08d8594351796b37dce4390d954ceb2
Luchoragusa <luchito.rc4@gmail.com>
2023-07-01 13:25:08 -0300

    exclude parameters -> getLocal

--- src/controllers/models/local.controller.js
+++ src/controllers/models/local.controller.js
@@ -14,7 +14,8 @@ const getLocals = async (req, res) => {
     }).then(async sucursal => {
       // Este metodo devuelve todas las sucursales de la empresa
       const sucursales = await Sucursal.findAll({
-        where: { cuilEmpresa: sucursal.Sucursal.cuilEmpresa }
+        where: { cuilEmpresa: sucursal.Sucursal.cuilEmpresa },
+        attributes: { exclude: ['createdAt', 'updatedAt', 'cuilEmpresa'] }
       })
       if (!sucursales) return res.status(404).json({ message: 'No se encontraron sucursales' })
       res.status(200).json({ sucursales })
