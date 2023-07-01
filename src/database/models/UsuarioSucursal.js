c96593918783eea2e2c4f17ad88b94403aa717c9
Luchoragusa <luchito.rc4@gmail.com>
2023-07-01 13:02:20 -0300

    add association

--- src/database/models/UsuarioSucursal.js
+++ src/database/models/UsuarioSucursal.js
@@ -3,6 +3,11 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class UsuarioSucursal extends Model {
     static associate (models) {
+      // Relacion con Usuario - La tabla UsuarioSucursal tiene FK de Usuario llamada idUsuario
+      UsuarioSucursal.belongsTo(models.Usuario, { foreignKey: 'idUsuario' })
+
+      // Relacion con Sucursal - La tabla UsuarioSucursal tiene FK de Sucursal llamada idSucursal
+      UsuarioSucursal.belongsTo(models.Sucursal, { foreignKey: 'idSucursal' })
     }
   }
   UsuarioSucursal.init({
