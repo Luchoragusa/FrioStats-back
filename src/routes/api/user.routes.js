43250156f3c2fae5fbce7ce7ee5e595e9a8c9404
Luchoragusa <luchito.rc4@gmail.com>
2023-06-30 23:40:06 -0300

    add res in login method

---
+++ src/routes/api/user.routes.js
@@ -0,0 +1,20 @@
+const Router = require('express')
+const router = Router()
+const { register, login } = require('../../controllers/models/user.controller')
+// const { validateToken, policy } = require('../../utilities/middleware')
+
+// Genericas
+// router.get('/', checkToken, getAll(User)); // muestra todos
+// router.get('/:id', checkToken, getOne(User)); // muestra uno
+// router.delete('/:id', checkToken, policy, deleteOne(User)); // borra uno
+
+// router.get('/', validateToken, policy, (req, res) => {
+//   res.status(200).json({ message: 'Hola' })
+// })
+
+//  Especificas
+router.post('/register', register) // Registrar un usuario en la DB
+router.post('/login', login) // crea uno
+// router.patch('/:id') // actualiza uno
+
+module.exports = router
