c3ff02fa1227adfd1769a75c47ffea2ccdced5f1
Luchoragusa <luchito.rc4@gmail.com>
2023-08-11 17:49:06 -0300

    update seed

--- src/database/seeders/20230512193243-MaquinaSucursal.js
+++ src/database/seeders/20230512193243-MaquinaSucursal.js
@@ -8,41 +8,48 @@ module.exports = {
       // Id 1
       idSucursal: 1,
       idModelo: 1,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       // Id 2
       idSucursal: 1,
       idModelo: 2,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       // Id 3
       idSucursal: 1,
       idModelo: 3,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       // Id 4
       idSucursal: 1,
       idModelo: 4,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       // Id 5
       idSucursal: 2,
       idModelo: 1,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       // Id 6
       idSucursal: 2,
       idModelo: 2,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       idSucursal: 2,
       idModelo: 3,
+      coords: 'POINT(-34.6037389 -58.3815704)',
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
