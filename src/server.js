586c39bc0926a4d65f462330d21ba4a37a114b4f
Luchoragusa <luchito.rc4@gmail.com>
2024-05-14 21:15:25 -0300

    desactive las mediciones

--- src/server.js
+++ src/server.js
@@ -22,17 +22,17 @@ const rule = new schedule.RecurrenceRule()
 // rule.second = times
 
 // Seteo cada 4 hora y modificando el i+=1 se puede modificar el intervalo
-const hours = []
-for (let i = 0; i <= 24; i += 4) {
-  hours.push(i)
-}
-rule.hour = hours
-rule.minute = 10
-rule.second = 0
+// const hours = []
+// for (let i = 0; i <= 24; i += 4) {
+//   hours.push(i)
+// }
+// rule.hour = hours
+// rule.minute = 10
+// rule.second = 0
 
-schedule.scheduleJob(rule, function () {
-  setTimeout(revisionMaquinas, 2500)
-})
+// schedule.scheduleJob(rule, function () {
+  // setTimeout(revisionMaquinas, 2500)
+// })
 
 // Settings
 app.use(cors())
