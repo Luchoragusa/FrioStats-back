e406c7de8150cd157d1123577536b3704541bc62
Luchoragusa <luchito.rc4@gmail.com>
2024-05-20 20:07:22 -0300

    /email/

--- src/routes/api/sucursal.routes.js
+++ src/routes/api/sucursal.routes.js
@@ -3,7 +3,7 @@ const router = Router()
 const Sucursalontroller = require('../../controllers/models/sucursal.controller')
 
 router.get('/:id', Sucursalontroller.getSucursal) 
-router.get('/:email', Sucursalontroller.getSucursalEmail) // Muestra las sucursales para ese email
+router.get('/email/:email', Sucursalontroller.getSucursalEmail) // Muestra las sucursales para ese email
 
 router.put('/', Sucursalontroller.updateUsuarioSucursal) // Actualiza la asignacion de la sucursal
 
